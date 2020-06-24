a6e772654af73c774ff2204be6cbf956566ec029
Camilo Gomez <camilogomezmusic@gmail.com>
2020-06-24 23:52:21 +0000

    Use a for loop to repeat a chorus line

---
+++ chorus-for.js
@@ -0,0 +1,12 @@
+const chorus = "Let's dance!";
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    console.log("*change key*");
+  }
+  
+  console.log(chorus);
+  
+}
+
+console.log("Until the sun comes up!");
